4180a4b283ff07f5c7347e31db000a88b7b1769d
kishman7 <kishman_us@ukr.net>
2021-07-21 23:29:52 +0300

    Add Homework_Kassa 2

--- Kassa/Form1.cs
+++ Kassa/Form1.cs
@@ -15,6 +15,37 @@ namespace Kassa
         public Form1()
         {
             InitializeComponent();
+            // за допомогою нашої функції змінюємо колір заголовку groupBox
+            GetColorTextGroupBox(groupBox1);
+            GetColorTextGroupBox(groupBox2);
+            GetColorTextGroupBox(groupBox3);
+            var naftoProduct = new[] {"Дизпаливо", "A-95", "A-92", "A-80" };
+            comboBox1.Items.AddRange(naftoProduct);
+            comboBox1.SelectedIndex = 0; //вибір елемента по замовчуванню
+
+        }
+
+        //Функція для зміни заголовка коліру groupBox, без зміни коліру тексту всіх Control, які є в groupBox
+        void GetColorTextGroupBox(GroupBox groupBox)
+        {
+            groupBox.ForeColor = Color.Blue;
+            foreach (Control item in groupBox.Controls)
+            {
+                item.ForeColor = SystemColors.ControlText;
+            }
+        }
+        double priceDT = 29.90; //ціна на ДТ
+        double priceA95 = 33.50; //ціна на A95
+        double priceA92 = 31.25; //ціна на A92
+        double priceA80 = 28.30; //ціна на A80
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                textBox1.Text = priceA95.ToString();
+            }
+
         }
     }
 }
